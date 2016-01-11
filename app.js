14808d9021fa2313b4f7b303851412d8ee932da0
Tom Fitzpatrick <tfitz237@gmail.com>
2016-01-11 16:22:02 -0500

    Cleaned up the code

--- app.js
+++ app.js
@@ -1,119 +1,117 @@
-console.log('////////////////////////////////////////////');
-console.log('          My apartment controller.');
-console.log('Setting up http server...');
 var gpio = require('rpi-gpio');
 var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
-console.log('Setting up motion sensor...');
-var motion = new Date();
-var motionOn = true;
-gpio.setMode(gpio.MODE_BCM);
-gpio.setup(19,gpio.DIR_IN,gpio.EDGE_BOTH);
-console.log('Motion sensor set up.');
-console.log('Starting web server...');
-server.listen(80,function(e){
-console.log('Server started. View at http://'+server.address().address);
-console.log('Awaiting input:');
-// motion sensor
-setInterval(checkSensor,60000);
-});
-gpio.on('change',function(channel,value) {
-	if(channel == 19) {
-		
-		console.log('Motion detected.');
-		motion = new Date();
-		if(lights[1].status == false && value == true && motionOn == true && (new Date().getHours() > 15 || new Date().getHours() < 3) ) {
-			// Turn on light only if the light is off, the motion sensor was tripped, and it's past 5pm
-			
-			sendCode(1);				
-		}
-	}
-});
-
-function checkSensor() {
-	console.log('Any motion in the past 5 minutes?');
-	gpio.read(19,function(err,value) {
-		if(value == false && lights[1].status == true && (new Date().getTime() - motion.getTime() > 300000) && motionOn == true) { 
-			sendCode(1);
-			console.log('No motion.');	
-		}
-		else {
-			console.log('Yes. Checking again in a minute.');
-		}
-	});
-	
-
-}
-
-
-app.use(express.static('public'));
-
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/public/outlet.html');
-});
-
 var sudo = require('sudo');
 var options = {
     cachePassword: true,
     prompt: 'Password, yo? ',
-    spawnOptions: { /* other options for spawn */ }
+    spawnOptions: {/* other options for spawn */}
 };
-var lights = [
- { 'id': 0, 'name': 'Bedroom Lights', 'status': true, 'code': [333116,333107] },   
- { 'id': 1, 'name': 'Living Room Lamp', 'status': false, 'code': [333260,333251] },  
- { 'id': 2, 'name': 'Audio Mixer', 'status': false, 'code': [333580,333571] },    
- { 'id': 3, 'name': 'Office Lamp', 'status': false, 'code': [341260,341251] }     
-];
-var motion = true;
+var outlet;
+server.listen(80, function (e) {
+    console.log('Server started. Awaiting Input:');
+    setInterval(checkSensor, 60000);
+});
+init();
+gpio.setMode(gpio.MODE_BCM);
+gpio.setup(19, gpio.DIR_IN, gpio.EDGE_BOTH);
+
+gpio.on('change', function (channel, value) {
+    if (channel == 19) {
+        console.log('Motion detected.');
+        outlet.motion = new Date();
+        if (outlet.lights[1].status == false && value == true)
+            if(outlet.motionOn == true && (outlet.motion.getHours() > 15 || outlet.motion.getHours() < 3))
+            // Turn on light only if the light is off, the motion sensor was tripped, and it's past 5pm
+            sendCode(1);
+
+    }
+});
 
 io.on('connection', connection);
 
+function init() {
+    outlet.motion = new Date();
+    outlet.motionOn = true;
 
-
+    outlet.lights = [
+        {'id': 0, 'name': 'Bedroom outlet.lights', 'status': false, 'code': [333116, 333107]},
+        {'id': 1, 'name': 'Living Room Lamp', 'status': false, 'code': [333260, 333251]},
+        {'id': 2, 'name': 'Audio Mixer', 'status': false, 'code': [333580, 333571]},
+        {'id': 3, 'name': 'Office Lamp', 'status': false, 'code': [341260, 341251]}
+    ];
+    app.use(express.static('public'));
+    app.get('/', function (req, res) {
+        res.sendFile(__dirname + '/public/outlet.html');
+    });
+}
 
 function connection(socket) {
-    console.log(socket.id);
-    io.emit('light status', lights);
-	io.emit('motion set', motionOn);    
+    console.log("User connected: " + socket.id);
+    io.emit('light status', outlet.lights);
+    io.emit('motion set', outlet.motionOn);
 
-	socket.on('motion set', function(n) { motionOn = n; console.log('Motion sensor set to ' + motionOn); io.emit('motion set', motionOn);});    
+    socket.on('motion set', function (n) {
+        outlet.motionOn = n;
+        console.log('Motion sensor set to ' + outlet.motionOn);
+        io.emit('motion set', outlet.motionOn);
+    });
 
-	socket.on('light status', function(n) { io.emit('light status', lights); io.emit('motion set', motionOn);});
+    socket.on('light status', function (n) {
+        io.emit('light status', outlet.lights);
+        io.emit('motion set', outlet.motionOn);
+    });
 
-    socket.on('light change', function(n){
-        
-        if(lights[n].status) {
-            console.log('Turning the ' + lights[n].name + ' off');
+    socket.on('light change', function (n) {
+
+        if (outlet.lights[n].status) {
+
             sendCode(n);
         }
         else {
-            console.log('Turning the ' + lights[n].name + ' on');
+            console.log('Turning the ' + outlet.lights[n].name + ' on');
             sendCode(n);
         }
-        io.emit('light status', lights);
-  });
-  
-  socket.on('disconnect', function(){});
-    
-    
-}
+        io.emit('light status', outlet.lights);
+    });
 
+    socket.on('disconnect', function () {
+    });
+}
 
 function sendCode(light) {
-	var code;
-	if(lights[light].status) {
-		code = lights[light].code[0]; lights[light].status = false;
-	}
-	else {
-		code = lights[light].code[1]; lights[light].status = true; 
-	}
-	io.emit('light status', lights); 
-    	var child = sudo([ '/var/www/rfoutlet/codesend', code.toString()], options);
-            child.stdout.on('data', function (data) {
-            console.log(data.toString());
-        });
-    
-    
+    var code;
+    var child = sudo(['/var/www/rfoutlet/codesend', code.toString()], options);
+    child.stdout.on('data', function (data) {
+        if (outlet.lights[light].status) {
+            code = outlet.lights[light].code[0];
+            outlet.lights[light].status = false;
+            console.log('Turning the ' + outlet.lights[n].name + ' off');
+        }
+        else {
+            code = outlet.lights[light].code[1];
+            outlet.lights[light].status = true;
+            console.log('Turning the ' + outlet.lights[n].name + ' on');
+        }
+        io.emit('light status', outlet.lights);
+    });
 }
+
+function checkSensor() {
+    if(outlet.motionOn == true) {
+        console.log('Any motion in the past 5 minutes?');
+        gpio.read(19, function (err, value) {
+            if (value == false && outlet.lights[1].status == true) {
+                if (new Date().getTime() - outlet.motion.getTime() > 300000) {
+                    sendCode(1);
+                    console.log('No motion.');
+                } else {
+                    console.log('Yes. Checking again in a minute.');
+                }
+            }
+        });
+    }
+}
+
--- public/app.js
+++ public/app.js
@@ -12,29 +12,25 @@
         $scope.lights = [];
         $scope.lightToggle = lightToggle;
         $scope.motionToggle = motionToggle;
-	$scope.motion = true;
-	socket.on('connect',function(data) {
-		socket.emit('light status');           
-            
-            
-            
+	    $scope.motionOn = true;
+	    socket.on('connect',function() {
+		    socket.emit('light status');
         });
         socket.on('light status', function (lights) {
             console.log(lights);
             $scope.lights = lights;
             $scope.$apply();
         });
-	socket.on('motion set', function (motionOn) {
-		$scope.motion = motionOn;
-	});        
+	    socket.on('motion set', function (value) {
+		    $scope.motionOn = value;
+            $scope.$apply();
+	    });
         function lightToggle(n) {
             socket.emit('light change', n);
-            
-            
         }
-	function motionToggle() {
-		socket.emit('motion set', $scope.motion);
-	}        
+	    function motionToggle() {
+		    socket.emit('motion set', $scope.motionOn);
+	    }
         
     }
     
